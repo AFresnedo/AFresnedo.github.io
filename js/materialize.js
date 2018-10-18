f2a057ec252c9159c54a4bfdcf30d9fc6ec81f9e
AFresnedo <AFresnedo@users.noreply.github.com>
2018-10-18 11:33:51 -0700

    demonstrate viability of modal insights in accordian projects

---
+++ js/materialize.js
@@ -0,0 +1,46 @@
+//
+// NOTE navbar (see materialze for setting options)
+//
+
+// sidebar activation, for navbar on mobile
+document.addEventListener('DOMContentLoaded', function() {
+  var elems = document.querySelectorAll('.sidenav');
+  var instances = M.Sidenav.init(elems);
+});
+
+// dropdown
+document.addEventListener('DOMContentLoaded', function() {
+  var elems = document.querySelectorAll('.dropdown-trigger');
+  var instances = M.Dropdown.init(elems);
+});
+
+//
+// NOTE sections
+//
+
+// accordian container
+document.addEventListener('DOMContentLoaded', function() {
+  var elems = document.querySelectorAll('.collapsible');
+  var instances = M.Collapsible.init(elems);
+});
+// insight reveal
+document.addEventListener('DOMContentLoaded', function() {
+  var elems = document.querySelectorAll('.modal');
+  var options = {
+    inDuration: 300, // Transition in duration
+    outDuration: 200, // Transition out duration
+    startingTop: '-10%', // Starting top style attribute
+    endingTop: '10px' // <-- HEIGHT OF THE BUTTON
+  }
+  var instances = M.Modal.init(elems, options);
+});
+
+//
+// NOTE background
+//
+
+// parallax backdrop
+document.addEventListener('DOMContentLoaded', function() {
+  var elems = document.querySelectorAll('.parallax');
+  var instances = M.Parallax.init(elems);
+});
